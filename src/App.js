db9c2e281f835d1bdc68af059a202ed17f2834f8
Damian Janus <damian.janus@gmail.com>
2021-02-18 21:18:36 +0100

    reveal and scale animation on image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import photo from './images/photo.webp';
+import { useEffect, useRef } from 'react';
+import { TimelineLite, Power2 } from "gsap";
+import CSSRulePlugin from 'gsap/CSSRulePlugin';
 
-function App() {
+const App = () => {
+  const imageRef = useRef(null);
+  const containerRef = useRef(null);
+  const imageReveal = CSSRulePlugin.getRule('.img-container:after');
+  const tl = new TimelineLite();
+
+  useEffect(() => {
+    tl.to(containerRef.current, 1, { css: { visibility: 'visible'} })
+    .to(imageReveal, 1.4, { width: '0%', ease: Power2.easeInOut })
+    .from(imageRef.current, 1.4, {scale: 1.6, ease: Power2.easeInOut, delay: -1.6 })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div ref={containerRef} className="container">
+        <>
+          <div className="img-container">
+            <img ref={imageRef} src={photo} alt="man in red jacket on the mountain" />
+          </div>
+        </>
+      </div>
     </div>
   );
 }
